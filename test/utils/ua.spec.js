7c368b3bce5217fe77394a17e3e26290087d18b6
duxiaonan <fiesta0210@sina.com>
2018-11-18 19:04:59 +0800

    travis-karma

--- test/utils/ua.spec.js
+++ test/utils/ua.spec.js
@@ -3,67 +3,30 @@
  * @author duxiaonan(duxiaonan@baidu.com)
  */
 
-// 模拟浏览器环境
-var requirejs = require('requirejs');
-requirejs.config({nodeRequire: require});
-// global.window = {
-//     navigator: {
-//         userAgent: 'fdasf'
-//     }
-// };
-// global.navigator = global.window.navigator;
-//
-// requirejs.config({
-//     baseUrl: ''
-// });
+define(function (require) {
+    var UA = require('utils/ua');
 
-requirejs(['../../utils/ua', 'chai'], function (UA, chai) {
     describe('utils/ua', function () {
-        describe('ua', function () {
-            it('should return isIOS boolean value', function () {
-                var ua = navigator.userAgent;
-                var isIOS = /(iPad|iPhone|iPod)/.test(ua);
-                chai.expect(UA.isIOS).to.be.equal(isIOS);
-            });
-            it('should return isWKWebView boolean value', function () {
-                var ua = navigator.userAgent;
-                var match = ua.match(/ applewebkit\/(\d+)/i);
-                var awVersion = match && parseFloat(match[1]);
-                var isWKWebView = +awVersion > 600;
-                chai.expect(UA.isWKWebView).to.be.equal(isWKWebView);
-            });
-            it('should return isBaidu boolean value', function () {
-                var ua = navigator.userAgent;
-                var isBaidu = /baiduboxapp|baiduhi/.test(ua);
-                chai.expect(UA.isBaidu).to.be.equal(isBaidu);
+        it('async load modules', function () {
+            describe('ua', function () {
+                it('should return isIOS boolean value', function () {
+                    var ua = navigator.userAgent;
+                    var isIOS = /(iPad|iPhone|iPod)/.test(ua);
+                    expect(UA.isIOS).to.be.equal(isIOS);
+                });
+                it('should return isWKWebView boolean value', function () {
+                    var ua = navigator.userAgent;
+                    var match = ua.match(/ applewebkit\/(\d+)/i);
+                    var awVersion = match && parseFloat(match[1]);
+                    var isWKWebView = +awVersion > 600;
+                    expect(UA.isWKWebView).to.be.equal(isWKWebView);
+                });
+                it('should return isBaidu boolean value', function () {
+                    var ua = navigator.userAgent;
+                    var isBaidu = /baiduboxapp|baiduhi/.test(ua);
+                    expect(UA.isBaidu).to.be.equal(isBaidu);
+                });
             });
         });
     });
 });
-
-// describe('utils/ua', function () {
-//     it('async load modules', function (done) {
-//         requirejs(['utils/ua', 'chai'], function (UA, chai) {
-//             describe('ua', function () {
-//                 it('should return isIOS boolean value', function () {
-//                     var ua = navigator.userAgent;
-//                     var isIOS = /(iPad|iPhone|iPod)/.test(ua);
-//                     chai.expect(UA.isIOS).to.be.equal(isIOS);
-//                 });
-//                 it('should return isWKWebView boolean value', function () {
-//                     var ua = navigator.userAgent;
-//                     var match = ua.match(/ applewebkit\/(\d+)/i);
-//                     var awVersion = match && parseFloat(match[1]);
-//                     var isWKWebView = +awVersion > 600;
-//                     chai.expect(UA.isWKWebView).to.be.equal(isWKWebView);
-//                 });
-//                 it('should return isBaidu boolean value', function () {
-//                     var ua = navigator.userAgent;
-//                     var isBaidu = /baiduboxapp|baiduhi/.test(ua);
-//                     chai.expect(UA.isBaidu).to.be.equal(isBaidu);
-//                 });
-//                 done();
-//             });
-//         });
-//     });
-// });
--- test/utils/url.spec.js
+++ test/utils/url.spec.js
@@ -3,65 +3,22 @@
  * @author duxiaonan(duxiaonan@baidu.com)
  */
 
-// 模拟浏览器环境
-var requirejs = require('requirejs');
-requirejs.config({nodeRequire: require});
-// global.window = {
-//     navigator: {
-//         userAgent: 'fdasf'
-//     },
-//     location: {
-//         href: 'fdas',
-//         search: 'fd'
-//     },
-//     document: {
-//         querySelector: function(){ return {} }
-//     }
-// };
-// global.location = global.window.location;
-//
-// requirejs.config({
-//     baseUrl: '',
-//     paths: {
-//         'ralltiir': 'amd_modules/ralltiir',
-//         '@searchfe/underscore': 'amd_modules/@searchfe/underscore',
-//         '@searchfe/assert': 'amd_modules/@searchfe/assert',
-//         '@searchfe/promise': 'amd_modules/@searchfe/promise'
-//     }
-// });
+define(function (require) {
+    var URL = require('utils/url');
 
-requirejs(['../../utils/url', 'chai'], function(URL, chai) {
     describe('utils/url', function() {
-        describe('#setQuery()', function () {
-            it('key should be equal to object', function () {
-                chai.expect(URL.setQuery('https://m.baidu.com/', {
-                    'rt-partial': 'true',
-                    'rt-selector': 'false'
-                })).to.be.equal('https://m.baidu.com/?rt-partial=true&rt-selector=false');
-            });
-            it('key should not be equal to object', function () {
-                chai.expect(URL.setQuery('https://m.baidu.com/', 'rt', 'true')).to.be.equal('https://m.baidu.com/?rt=true');
+        it('async load modules', function () {
+            describe('#setQuery()', function () {
+                it('key should be equal to object', function () {
+                    expect(URL.setQuery('https://m.baidu.com/', {
+                        'rt-partial': 'true',
+                        'rt-selector': 'false'
+                    })).to.be.equal('https://m.baidu.com/?rt-partial=true&rt-selector=false');
+                });
+                it('key should not be equal to object', function () {
+                    expect(URL.setQuery('https://m.baidu.com/', 'rt', 'true')).to.be.equal('https://m.baidu.com/?rt=true');
+                });
             });
         });
     });
 });
-
-// describe('utils/url', function() {
-//     it('async load modules', function (done) {
-//         requirejs(['utils/url', 'chai'], function(URL, chai) {
-//             describe('#setQuery()', function () {
-//                 it('key should be equal to object', function () {
-//                     chai.expect(URL.setQuery('https://m.baidu.com/', {
-//                         'rt-partial': 'true',
-//                         'rt-selector': 'false'
-//                     })).to.be.equal('https://m.baidu.com/?rt-partial=true&rt-selector=false');
-//                 });
-//                 it('key should not be equal to object', function () {
-//                     chai.expect(URL.setQuery('https://m.baidu.com/', 'rt', 'true')).to.be.equal('https://m.baidu.com/?rt=true');
-//                 });
-//                 done();
-//             });
-//         });
-//     });
-// });
-
